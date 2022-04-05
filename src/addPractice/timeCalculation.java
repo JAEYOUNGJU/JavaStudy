ac54c55af5193d4246a7cad665b14c7363751825
jujaeyoung <jujy4315@gmail.com>
2022-04-05 13:04:56 +0900

    시간차이 계산

--- src/addPractice/timeCalculation.java
+++ src/addPractice/timeCalculation.java
@@ -1,10 +1,52 @@
 package addPractice;
 
+import java.util.Scanner;
+
 public class timeCalculation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+			Scanner sc = new Scanner(System.in);
+			
+			
+			//문자열-> 문자 걸러내기
+			int firstHour = sc.nextInt();
+			int firstMinute = sc.nextInt();
+			int firstSecond = sc.nextInt();
+			int secondHour = sc.nextInt();
+			int secondMinute = sc.nextInt();
+			int secondSecond = sc.nextInt();
+			
+			System.out.println(firstHour + "h " + firstMinute + "m " + firstSecond + "s");
+			System.out.println(secondHour + "h " + secondMinute + "m " + secondSecond + "s");
+			
+			firstHour = firstHour*60*60;
+			firstMinute = firstMinute*60;
+			secondHour = secondHour*60*60;
+			secondMinute = secondMinute*60;
+			
+			int num1 = firstHour+firstMinute+firstSecond;
+			int num2 = secondHour+secondMinute+secondSecond;
 
-	}
+			int bignumber = 0;
+			int smallnumber = 0;
+			
+			if(num1 > num2) {
+				bignumber = num1;
+				smallnumber = num2;
+			} else {
+				bignumber = num2;
+				smallnumber = num1;
 
-}
+			}
+			
+			int sub = bignumber - smallnumber;
+			
+			int hour1= sub/3600;
+			int firstlast = sub % 3600;
+			int minute1 = firstlast/60;
+			int second1 = firstlast % 60;
+			
+			System.out.println(hour1 + "h "+ minute1 + "m " + second1 + "s");
+			}
+		}
